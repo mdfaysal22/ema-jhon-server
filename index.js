09d020645a0a5a2ad7e3dd0e9cf54579a2bba998
mdfaysal22 <mdfaysal.freelancer@gmail.com>
2023-04-26 11:09:21 +0600

    All

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // Middleware 
@@ -9,6 +10,49 @@ app.use(express.json());
 
 // Environment Variable 
 require('dotenv').config();
+
+
+// Mongodb Connection ---- >>> 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k8emzbd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const productCollection = client.db('emaJhon').collection('products')
+
+        // Create API
+        app.get('/products', async(req, res) => {
+            // For get All Data from DB
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.skip(page*size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count,products})
+
+        })
+
+        // Get Request >>>> 
+        app.post('/productsByIds', async(req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => ObjectId(id))
+            const query = {_id : {$in : objectIds}};
+            const cursor = productCollection.find(query);
+            const product = await cursor.toArray();
+            res.send(product);
+        })
+    }
+    catch{}
+}
+
+run().catch(err => console.log(err))
+
+
+
+
+
+
 
 
 app.get('/', (req, res) =>{
